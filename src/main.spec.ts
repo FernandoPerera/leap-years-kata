05a72ace2983945b902739f4ab9c584e8a28c225
Fernando Perera <fernando02perera@gmail.com>
2023-11-20 23:41:49 +0100

    :memo: added test use cases

--- src/main.spec.ts
+++ src/main.spec.ts
@@ -1,6 +1,16 @@
 import { describe, it, expect } from "vitest"
 import { isLeadYear } from "./main.js"
 
+/*
+ * Use cases
+ * 
+ *  - isLeapYear(8)     => true      [divisible by 4]
+ *  - isLeapYear(2008)  => true      [divisible by 4 but not by 100]
+ *  - isLeapYear(2000)  => true      [divisible by 400]
+ * 
+ *  - isLeapYear(1800)  => false     [divisible by 4 but not by 400]
+*/
+
 describe("Lead year function test suit", () => {
   
 })
