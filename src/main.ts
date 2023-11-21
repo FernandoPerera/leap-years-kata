a6296c89fb3cbb3a2f1b7c95674ef1c2beceab40
Fernando Perera <fernando02perera@gmail.com>
2023-11-21 23:08:24 +0100

    ♻️ added declarative name when year is atypical leap year

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 export function isLeadYear(year: number): boolean {
 
-  if (year % 100 === 0 && year % 400 !== 0) { return false }
+  const isAtypicalLeapYear: boolean = year % 100 === 0 && year % 400 !== 0
+  if (isAtypicalLeapYear) { return false }
 
   const isDivisibleByFour: boolean = year % 4 === 0
   if (isDivisibleByFour) { return true }
