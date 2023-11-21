2514205fefcf0f6fbe54e3389a6ebcfa192e501a
Fernando Perera <fernando02perera@gmail.com>
2023-11-21 22:40:17 +0100

    🚨 should not be leap year if is not divisible by 4 test failed, not implemented yet

--- src/main.spec.ts
+++ src/main.spec.ts
@@ -4,13 +4,21 @@ import { isLeadYear } from "./main.js"
 /*
  * Use cases
  * 
- *  - isLeapYear(8)     => true      [divisible by 4]
  *  - isLeapYear(2008)  => true      [divisible by 4 but not by 100]
  *  - isLeapYear(2000)  => true      [divisible by 400]
  * 
+ *  - isLeapYear(7)     => false     [divisible by 4]
  *  - isLeapYear(1800)  => false     [divisible by 4 but not by 400]
 */
 
 describe("Lead year function test suit", () => {
-  
+
+    it("should not be leap year if is not divisible by 4", () => {
+        const year: number = 7
+
+        const result: boolean = isLeadYear(year)
+    
+        expect(result).toBe(false)
+    })
+    
 })
